0616e35e647198a715a5d574e52193a5d27de97b
0-kaladin <0.kaladin@gmail.com>
2013-09-21 10:57:17 -0400

    missed a flag

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -165,7 +165,9 @@ LOCAL_SHARED_LIBRARIES += \
         libstagefright_foundation \
         libdl
 
-LOCAL_CFLAGS += -Wno-multichar
+LOCAL_CFLAGS += \
+        -Wno-multichar \
+        -Wno-error=strict-aliasing
 
 ifeq ($(BOARD_USE_SAMSUNG_COLORFORMAT), true)
 LOCAL_CFLAGS += -DUSE_SAMSUNG_COLORFORMAT
